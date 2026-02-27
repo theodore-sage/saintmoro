fff68bb52e378af9d303d3ae9748b308f672bccb
theodore-sage <theosage.moro@gmail.com>
2026-02-27 13:08:35 -0800

    first smart contract

--- Solidity Development Course/Course 1/SimpleStorage.sol
+++ Solidity Development Course/Course 1/SimpleStorage.sol
@@ -22,5 +22,25 @@ contract SimpleStorage {
     // contract is a keyword that tells Solidity the name of the smart contract.
     // The 'contract' keyword can be related to a class in JavaScript or Python
 
-    
-}
+    // Solidity has different 'types' of values used to create variables. 
+    // Some of the basic types include:
+
+    // bool: true or false
+    // uint256 : unsigned integer, always positive; maximum of 256 bits/128 bytes
+    // int128 : integers, positive or negative; maximium of 128 bits/64 bytes
+    // address: Ethereum address, 42 hex characters long
+    // string: a text-value variable representing a bytes(2-32) type;
+    // bytes: a low-level value for EVM 
+
+    bool hasFavoriteNumber = true;
+    uint256 favoriteNumber = 39;
+    string favoriteNumberInText = "thirty-nine";
+    int256 favoriteInt = -39;
+    address myDevAddress = 0x116A6bc6D22e151eB34B346Dc1BD9b907F54faD1;
+
+    // Solidity is an explicit-preferred programming language. With uint and int types, it is
+    // best practice to include how many bits the variable type is limited to.
+
+    // Variables in Solidity can be initialized without initially being assigned a value, similar to
+    // { let name } in JavaScript
+};
