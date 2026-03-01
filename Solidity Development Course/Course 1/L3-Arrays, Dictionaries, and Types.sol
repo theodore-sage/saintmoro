c7dbb314c59e15f03a4f0cc977d1d90b48fcda3a
theodore-sage <theosage.moro@gmail.com>
2026-03-01 13:05:04 -0800

    added new function to lesson 3

--- Solidity Development Course/Course 1/L3-Arrays, Dictionaries, and Types.sol
+++ Solidity Development Course/Course 1/L3-Arrays, Dictionaries, and Types.sol
@@ -22,6 +22,38 @@ contract SimpleListStorage {
 
      Person public Teddy = Person(39, "Teddy");
 
-    // This variable will want to be externally facing when called, so 'public' is included after
-    // declaring the type
+    // This variable will want to be externally facing so it can be called, 
+    // so 'public' is included after declaring the type.
+
+    // When adding multiple variables, they can be created as a new array after constructing the new type:
+    Person[] public listOfPeople; 
+
+    // Above, the command creates a new array of 'People' saved as the variable 'listOfPeople', and 
+    // its visibility as set as public so it can be viewed and interacted with when the smart contract
+    // is deployed. There is currently no limit to how many variables can be saved in the array
+
+    // There are two types of arrays in Solidity,
+    // a dynamic array: Person[], which has no maximum limit of values that can be saved
+    // a static array: Person[3], which has a maximium limit of values that can be saved, specified by the number within the square brackets
+
+    // -------------------
+
+    // Because smart contracts are to have interaction, a function is created below so we can add new values
+    // to the smart contract after it has been deployed, rather than declaring them directly from the function.
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+         // This function will be called when we want to add a new person to the list of people
+         // This function will take two arguments, a string and a uint256
+
+         // When "memory" is added, this will temporarily save the argument passed into the function
+         // so it can be written into the smart contract. Once the function has been interacted with,
+         // the argument passed through "memory" will no longer be accessible by the function, but will be
+         // written into the data held by the smart contract.
+
+        listOfPeople.push(Person(_favoriteNumber, _name));
+
+        // Arrays have a built-in function, "push()", that can append the new data to the array.
+        // Solidity is intuitive that it will recognize the argument types declared within the function,
+        // and pass them into the type argument specified within the function.
+    }
 }
